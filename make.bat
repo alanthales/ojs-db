cbb1856faa4ec8e58cd87f289bc2771ebfaa4f34
Alan Thales <alanthales@gmail.com>
2016-03-23 17:52:43 -0300

    added apache license

--- make.bat
+++ make.bat
@@ -2,4 +2,4 @@
 
 echo compiling...
 
-uglifyjs src\HashMap.js src\DbProxy.js src\LocalStorageProxy.js src\SQLiteProxy.js src\RestProxy.js src\SyncDb.js src\DataSet.js src\DbFactory.js -o ojs-db.min.js -m -r 'HashMap,DbProxies,DbProxy,LocalStorageProxy,SQLiteProxy,RestProxy,SyncDb,DataSet,DbFactory' -c
+uglifyjs src\HashMap.js src\DbProxy.js src\LocalStorageProxy.js src\SQLiteProxy.js src\RestProxy.js src\SyncDb.js src\DataSet.js src\DbFactory.js -o ojs-db.min.js -m -r 'HashMap,DbProxies,DbProxy,LocalStorageProxy,SQLiteProxy,RestProxy,SyncDb,DataSet,DbFactory' -c --comments
